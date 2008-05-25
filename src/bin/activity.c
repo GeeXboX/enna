a7c273f7ce59c4a8a5ee3a2471f21e192e2f0ba1
gxben <devnull@localhost>
2008-05-25 19:35:05 +0200

    [svn] factorizes activity retrieval

--- src/bin/activity.c
+++ src/bin/activity.c
@@ -62,105 +62,95 @@ enna_activities_get(void)
   return _enna_activities;
 }
 
-
-EAPI int
-enna_activity_init(const char *name)
+static Enna_Class_Activity *
+enna_get_activity (const char *name)
 {
   Evas_List *l;
   Enna_Class_Activity *act;
 
-  if (!name) return -1;
+  if (!name)
+    return NULL;
 
   for (l = _enna_activities; l; l = l->next)
-     {
-	act = l->data;
-	if (!strcmp(act->name, name))
-	  {
-              if (act->func.class_init)
-                  act->func.class_init(0);
-	  }
-     }
+  {
+    act = l->data;
+    if (!act)
+      continue;
+    
+    if (act->name && !strcmp (act->name, name))
+      return act;
+  }
+
+  return NULL;
+}
+
+EAPI int
+enna_activity_init(const char *name)
+{
+  Enna_Class_Activity *act;
+
+  act = enna_get_activity (name);
+  if (!act) return -1;
+
+  if (act->func.class_init)
+    act->func.class_init(0);
+
   return 0;
 }
 
 EAPI int
 enna_activity_show(const char *name)
 {
-  Evas_List *l;
   Enna_Class_Activity *act;
 
-  if (!name) return -1;
+  act = enna_get_activity (name);
+  if (!act) return -1;
 
-  for (l = _enna_activities; l; l = l->next)
-     {
-	act = l->data;
-	if (!strcmp(act->name, name))
-	  {
-	    if (act->func.class_show)
-	      act->func.class_show(0);
-	  }
-     }
+  if (act->func.class_show)
+    act->func.class_show(0);
+
   return 0;
 }
 
 EAPI int
 enna_activity_shutdown(const char *name)
 {
-  Evas_List *l;
   Enna_Class_Activity *act;
 
-  if (!name) return -1;
+  act = enna_get_activity (name);
+  if (!act) return -1;
 
-  for (l = _enna_activities; l; l = l->next)
-     {
-	act = l->data;
-	if (!strcmp(act->name, name))
-	  {
-	    if (act->func.class_shutdown)
-                act->func.class_shutdown(0);
-	  }
-     }
+  if (act->func.class_shutdown)
+    act->func.class_shutdown(0);
+
   return 0;
 }
 
 EAPI int
 enna_activity_hide(const char *name)
 {
-  Evas_List *l;
   Enna_Class_Activity *act;
 
-  if (!name) return -1;
+  act = enna_get_activity (name);
+  if (!act) return -1;
 
-  for (l = _enna_activities; l; l = l->next)
-     {
-	act = l->data;
-	if (!strcmp(act->name, name))
-	  {
-	    if (act->func.class_hide)
-	      act->func.class_hide(0);
-	  }
-     }
+  if (act->func.class_hide)
+    act->func.class_hide(0);
+
   return 0;
 }
 
 EAPI int
 enna_activity_event(const char *name, void *event_info)
 {
-  Evas_List *l;
   Enna_Class_Activity *act;
 
+  act = enna_get_activity (name);
+  if (!act) return -1;
 
-  if (!name) return -1;
+  if (act->func.class_event)
+    act->func.class_event(event_info);
 
-  for (l = _enna_activities; l; l = l->next)
-     {
-	act = l->data;
-	if (!strcmp(act->name, name))
-	  {
-	    if (act->func.class_event)
-	      act->func.class_event(event_info);
-	  }
-     }
   return 0;
 }
 
