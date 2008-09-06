dd9e1263a36bfe5dd083c3281e3c56672659d3cf
Benjamin Zores <ben@geexbox.org>
2008-09-07 00:28:15 +0200

    remove deprecated dbg() macros

--- src/bin/enna.h
+++ src/bin/enna.h
@@ -37,24 +37,6 @@
 #define PATH_MAX 4096
 #endif
 
-
-
-#if ENNA_DEBUG == 2
-# define dbg(fmt,arg...) fprintf(stderr, "[Enna] [%s/%d] " fmt,__FILE__,__LINE__,##arg)
-#elif ENNA_DEBUG == 1
-# define dbg(fmt,arg...) fprintf(stderr, "[Enna] " fmt,##arg)
-#else
-# define dbg(fmt,arg...) do { } while(0)
-#endif
-
-#if ENNA_DEBUG == 2
-# define dbg_thm(fmt,arg...) fprintf(stderr, "[Enna Edje] [%s/%d] " fmt,__FILE__,__LINE__,##arg)
-#elif ENNA_DEBUG == 1
-# define dbg_thm(fmt,arg...) fprintf(stderr, "[Enna Edje] " fmt,##arg)
-#else
-# define dbg_thm(fmt,arg...) do { } while(0)
-#endif
-
 #define NBR_ELEMENTS(arr)		(sizeof (arr) / sizeof ((arr)[0]))
 #define ENNA_FREE(p) { if (p) {free(p); p = NULL;} }
 #define ENNA_NEW(s, n) (s *)calloc(n, sizeof(s))
