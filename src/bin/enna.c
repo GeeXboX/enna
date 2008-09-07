a1bc877d92de6a8ef110a1b06a415c378eba780b
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2008-09-07 09:36:17 +0200

    cosmetics: reindent

--- src/bin/enna.c
+++ src/bin/enna.c
@@ -166,18 +166,18 @@ _enna_init(int run_gl)
 
    if (enna_config->verbosity)
      {
-   if (!strcmp (enna_config->verbosity, "none"))
-     enna->lvl = ENNA_MSG_NONE;
-   else if (!strcmp (enna_config->verbosity, "event"))
-     enna->lvl = ENNA_MSG_EVENT;
-   else if (!strcmp (enna_config->verbosity, "info"))
-     enna->lvl = ENNA_MSG_INFO;
-   else if (!strcmp (enna_config->verbosity, "warning"))
-     enna->lvl = ENNA_MSG_WARNING;
-   else if (!strcmp (enna_config->verbosity, "error"))
-     enna->lvl = ENNA_MSG_ERROR;
-   else if (!strcmp (enna_config->verbosity, "critical"))
-     enna->lvl = ENNA_MSG_CRITICAL;
+        if (!strcmp (enna_config->verbosity, "none"))
+          enna->lvl = ENNA_MSG_NONE;
+        else if (!strcmp (enna_config->verbosity, "event"))
+          enna->lvl = ENNA_MSG_EVENT;
+        else if (!strcmp (enna_config->verbosity, "info"))
+          enna->lvl = ENNA_MSG_INFO;
+        else if (!strcmp (enna_config->verbosity, "warning"))
+          enna->lvl = ENNA_MSG_WARNING;
+        else if (!strcmp (enna_config->verbosity, "error"))
+          enna->lvl = ENNA_MSG_ERROR;
+        else if (!strcmp (enna_config->verbosity, "critical"))
+          enna->lvl = ENNA_MSG_CRITICAL;
      }
 
    if (!strcmp(enna_config->engine, "gl") && ecore_evas_engine_type_supported_get(ECORE_EVAS_ENGINE_OPENGL_X11))
