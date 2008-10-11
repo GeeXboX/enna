b7446ec7373fcd7cebb10d698223090467463277
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2008-10-11 16:11:33 +0200

    First version of picture wall

---
+++ src/bin/wall.h
@@ -0,0 +1,8 @@
+#ifndef _ENNA_WALL_H
+#define _ENNA_WALL_H
+
+#include "enna.h"
+
+EAPI Evas_Object *enna_wall_add(Evas * evas);
+EAPI void enna_wall_picture_append(Evas_Object *obj, const char *filename);
+#endif
