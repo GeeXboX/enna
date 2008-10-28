0edd248e38e4b999ce46353d0df3315579dd2721
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2008-10-29 00:02:46 +0100

    Remove cdcase and use shadow instead

--- src/modules/music/smart_player.c
+++ src/modules/music/smart_player.c
@@ -143,6 +143,7 @@ void enna_smart_player_metadata_set(Evas_Object *obj,
         /* FIXME : add edje cb at end of cover transition to switch properly covers*/
         sd->o_cover_old = sd->o_cover;
         sd->o_cover = enna_image_add(evas_object_evas_get(sd->o_edje));
+	enna_image_fill_inside_set(sd->o_cover, 0);
         enna_image_file_set(sd->o_cover, cover_file);
         edje_object_part_swallow(sd->o_edje, "enna.swallow.cover", sd->o_cover);
         edje_object_signal_emit(sd->o_edje, "cover,show", "enna");
