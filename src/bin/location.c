952386b6052595697a1f4c887cab2183e93c9743
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2008-11-03 22:42:14 +0100

    remove vertical scrollbar of location widget

--- src/bin/location.c
+++ src/bin/location.c
@@ -258,8 +258,8 @@ static void _smart_add(Evas_Object * obj)
     sd->h = 0;
     sd->items = NULL;
     sd->o_scroll = enna_scrollframe_add(evas_object_evas_get(obj));
-    enna_scrollframe_policy_set(sd->o_scroll, ENNA_SCROLLFRAME_POLICY_AUTO,
-            ENNA_SCROLLFRAME_POLICY_AUTO);
+    enna_scrollframe_policy_set(sd->o_scroll, ENNA_SCROLLFRAME_POLICY_OFF,
+            ENNA_SCROLLFRAME_POLICY_OFF);
     enna_scrollframe_child_set(sd->o_scroll, sd->o_box);
     sd->smart_obj = obj;
     evas_object_smart_member_add(sd->o_scroll, obj);
