a8de98c9eaf916141b00ecb7f1d49e18652d8cfc
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2008-12-07 18:40:42 +0100

    Bad copy/paste

--- src/bin/wall.h
+++ src/bin/wall.h
@@ -3,7 +3,7 @@
 
 #include "enna.h"
 
-Evas_Object *walle_wall_add(Evas * evas);
+Evas_Object *enna_wall_add(Evas * evas);
 void enna_wall_picture_append(Evas_Object *obj, const char *filename);
 void enna_wall_select_nth(Evas_Object *obj, int col, int row);
 void enna_wall_left_select(Evas_Object *obj);
