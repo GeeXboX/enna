bc90a601792d9e659b389a054a4e559d35c80e94
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2008-12-21 00:18:27 +0100

    Remove unused variable

--- src/modules/activity/music/enna_module_music.c
+++ src/modules/activity/music/enna_module_music.c
@@ -37,7 +37,6 @@ enum _MUSIC_STATE
 
 struct _Enna_Module_Music
 {
-    Evas *e;
     Evas_Object *o_edje;
     Evas_Object *o_list;
     Evas_Object *o_browser;
@@ -128,8 +127,7 @@ _class_event(void *event_info)
 	if (mod->o_mediaplayer)
 	{
 	    ENNA_TIMER_DEL(mod->timer_show_mediaplayer);
-	    mod->timer_show_mediaplayer = ecore_timer_add(10,
-		_show_mediaplayer_cb, NULL);
+	    mod->timer_show_mediaplayer = ecore_timer_add(10,_show_mediaplayer_cb, NULL);
 	}
 	switch (key)
 	{
@@ -151,8 +149,7 @@ _class_event(void *event_info)
 	if (mod->o_mediaplayer)
 	{
 	    ENNA_TIMER_DEL(mod->timer_show_mediaplayer);
-	    mod->timer_show_mediaplayer = ecore_timer_add(10,
-		_show_mediaplayer_cb, NULL);
+	    mod->timer_show_mediaplayer = ecore_timer_add(10,_show_mediaplayer_cb, NULL);
 	}
 	enna_browser_event_feed(mod->o_browser, event_info);
 	break;
@@ -171,11 +168,9 @@ _class_event(void *event_info)
 	    break;
 	case ENNA_KEY_CANCEL:
 	    ENNA_TIMER_DEL(mod->timer_show_mediaplayer);
-	    mod->timer_show_mediaplayer = ecore_timer_add(10,
-		_show_mediaplayer_cb, NULL);
+	    mod->timer_show_mediaplayer = ecore_timer_add(10,_show_mediaplayer_cb, NULL);
 	    enna_log(ENNA_MSG_INFO, ENNA_MODULE_NAME, "Add timer 10s");
-	    edje_object_signal_emit(mod->o_edje, "mediaplayer,hide",
-		"enna");
+	    edje_object_signal_emit(mod->o_edje, "mediaplayer,hide","enna");
 	    edje_object_signal_emit(mod->o_edje, "content,show", "enna");
 	    if (mod->o_browser)
 		mod->state = BROWSER_VIEW;
