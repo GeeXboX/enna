eae46bf02eb941ecde6ca5877e83a2d7cc4d3f87
Benjamin Zores <ben@geexbox.org>
2009-03-01 00:20:25 +0100

    add a new encoding error resiliant xml opening function

--- src/bin/xml_utils.c
+++ src/bin/xml_utils.c
@@ -1,6 +1,20 @@
 #include "enna.h"
 #include "xml_utils.h"
 
+xmlDocPtr
+get_xml_doc_from_memory (char *buffer)
+{
+    xmlDocPtr doc = NULL;
+
+    if (!buffer)
+        return NULL;
+
+    doc = xmlReadMemory (buffer, strlen (buffer), NULL, NULL,
+                         XML_PARSE_RECOVER |
+                         XML_PARSE_NOERROR | XML_PARSE_NOWARNING);
+    return doc;
+}
+
 xmlNode * get_node_xml_tree(xmlNode *root, const char *prop)
 {
     xmlNode *n, *children_node;
--- src/bin/xml_utils.h
+++ src/bin/xml_utils.h
@@ -4,6 +4,7 @@
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
+xmlDocPtr get_xml_doc_from_memory (char *buffer);
 xmlNode *get_node_xml_tree(xmlNode *root, const char *prop);
 xmlChar *get_prop_value_from_xml_tree(xmlNode *root, const char *prop);
 xmlChar *get_prop_value_from_xml_tree_by_attr (xmlNode *root, const char *prop,
