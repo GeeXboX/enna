f8312a787c8da02b3ee5e711f3c4c5338094eaca
Benjamin Zores <ben@geexbox.org>
2009-03-30 22:44:50 +0200

    move functions to avoid prototypes declaration

--- src/bin/mediacontrol.c
+++ src/bin/mediacontrol.c
@@ -64,16 +64,6 @@ struct _Smart_Data
 
 /* local subsystem functions */
 static void _smart_reconfigure(Smart_Data * sd);
-static void _smart_init(void);
-static void _smart_add(Evas_Object * obj);
-static void _smart_del(Evas_Object * obj);
-static void _smart_move(Evas_Object * obj, Evas_Coord x, Evas_Coord y);
-static void _smart_resize(Evas_Object * obj, Evas_Coord w, Evas_Coord h);
-static void _smart_show(Evas_Object * obj);
-static void _smart_hide(Evas_Object * obj);
-static void _smart_color_set(Evas_Object * obj, int r, int g, int b, int a);
-static void _smart_clip_set(Evas_Object * obj, Evas_Object * clip);
-static void _smart_clip_unset(Evas_Object * obj);
 
 /*event from Media player*/
 static int _start_cb(void *data, int type, void *event);
@@ -90,15 +80,6 @@ static void show_pause_button(Smart_Data * sd);
 /* local subsystem globals */
 static Evas_Smart *_smart = NULL;
 static Enna_Playlist *_enna_playlist;
-
-/* externally accessible functions */
-Evas_Object *
-enna_mediacontrol_add(Evas * evas,Enna_Playlist *enna_playlist)
-{
-    _enna_playlist=enna_playlist;
-    _smart_init();
-    return evas_object_smart_add(evas, _smart);
-}
 
 /* Event from mediaplayer*/
 static int
@@ -238,30 +219,6 @@ _smart_reconfigure(Smart_Data * sd)
     evas_object_move(sd->o_edje, x, y);
     evas_object_resize(sd->o_edje, w, h);
 
-}
-
-static void
-_smart_init(void)
-{
-    if (_smart)
-        return;
-    static const Evas_Smart_Class sc =
-    {
-        SMART_NAME,
-        EVAS_SMART_CLASS_VERSION,
-        _smart_add,
-        _smart_del,
-        _smart_move,
-        _smart_resize,
-        _smart_show,
-        _smart_hide,
-        _smart_color_set,
-        _smart_clip_set,
-        _smart_clip_unset,
-        NULL,
-        NULL
-    };
-    _smart = evas_smart_class_new(&sc);
 }
 
 static void
@@ -479,4 +436,37 @@ _smart_clip_unset(Evas_Object * obj)
 {
     INTERNAL_ENTRY;
     evas_object_clip_unset(sd->o_edje);
+}
+
+static void
+_smart_init(void)
+{
+    if (_smart)
+        return;
+    static const Evas_Smart_Class sc =
+    {
+        SMART_NAME,
+        EVAS_SMART_CLASS_VERSION,
+        _smart_add,
+        _smart_del,
+        _smart_move,
+        _smart_resize,
+        _smart_show,
+        _smart_hide,
+        _smart_color_set,
+        _smart_clip_set,
+        _smart_clip_unset,
+        NULL,
+        NULL
+    };
+    _smart = evas_smart_class_new(&sc);
+}
+
+/* externally accessible functions */
+Evas_Object *
+enna_mediacontrol_add(Evas * evas,Enna_Playlist *enna_playlist)
+{
+    _enna_playlist=enna_playlist;
+    _smart_init();
+    return evas_object_smart_add(evas, _smart);
 }
