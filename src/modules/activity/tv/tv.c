3a17fb813c122b5a286467f16d358d7978b67ebf
Davide Cavalca <davide@geexbox.org>
2009-03-30 18:47:06 +0200

    rework VDR keymap

--- src/modules/activity/tv/tv.c
+++ src/modules/activity/tv/tv.c
@@ -86,7 +86,7 @@ static void _class_event(void *event_info)
 
     switch (key)
     {
-        case ENNA_KEY_CANCEL:
+        case ENNA_KEY_MENU:
             enna_content_hide();
             enna_mainmenu_show(enna->o_mainmenu);
             break;
