b62b48e412495e31de186d71cebfb80ef12f2721
Benjamin Zores <ben@geexbox.org>
2009-05-07 00:09:15 +0200

    set metadata debug level back to 0 again (badly replaced by tvrage commit)

--- src/bin/metadata.c
+++ src/bin/metadata.c
@@ -50,7 +50,7 @@
 #define PATH_METADATA           "metadata"
 #define PATH_SNAPSHOTS          "snapshots"
 
-#define DEBUG 1
+#define DEBUG 0
 #define EET_DO_COMPRESS 1
 
 static Eina_List *metadata_grabbers = NULL;
