1d7ba9b321dd1eedb59313efca0902b863fe3173
N-Billy <n-billy@geexbox.org>
2009-05-17 17:49:20 +0200

    remove logging in case locale couln't be set

--- src/bin/utils.c
+++ src/bin/utils.c
@@ -326,9 +326,6 @@ md5sum (char *str)
 char *init_locale (void)
 {
     char *curlocale=setlocale(LC_ALL, "");
-    //FIXME: currently, logging will fail here 'cause log level isn't initialized yet but when called after that, help wouldn't get translated
-    if (!curlocale)
-        enna_log(ENNA_MSG_WARNING, NULL, "unable to set locale, using 'C' instead.");
     mylocale = curlocale?strdup(curlocale):strdup(setlocale(LC_ALL, "C"));
     bindtextdomain(PACKAGE, LOCALEDIR);
     textdomain(PACKAGE);
