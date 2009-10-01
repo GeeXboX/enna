e095308ba95302f5678e5fbacccf0fe24e6f2ded
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2009-10-02 00:20:38 +0200

    simplify lyrics panel, theme is not what is expected, but i'll do that later.

--- src/modules/activity/music/panel_lyrics.c
+++ src/modules/activity/music/panel_lyrics.c
@@ -37,168 +37,47 @@
 #include "image.h"
 #include "buffer.h"
 
-#define SMART_NAME "enna_panel_lyrics"
 
 typedef struct _Smart_Data Smart_Data;
 
 struct _Smart_Data
 {
-    Evas_Coord x, y, w, h;
-    Evas_Object *o_edje;
-    Evas_Object *o_scroll;
-    Evas_Object *o_text;
+    Evas_Object *scroller;
+    Evas_Object *text;
 };
 
 /* local subsystem globals */
-static Evas_Smart *_smart = NULL;
 
 /* local subsystem globals */
-static void
-_smart_reconfigure (Smart_Data *sd)
-{
-    Evas_Coord x, y, w, h;
 
-    x = sd->x;
-    y = sd->y;
-    w = sd->w;
-    h = sd->h;
-
-    evas_object_move (sd->o_edje, x, y);
-    evas_object_resize (sd->o_edje, w, h);
-}
-
-static void
-_smart_add (Evas_Object *obj)
-{
-    Smart_Data *sd;
-
-    sd = calloc (1, sizeof (Smart_Data));
-    if (!sd)
-        return;
-
-    sd->o_edje = edje_object_add (evas_object_evas_get (obj));
-    edje_object_file_set (sd->o_edje, enna_config_theme_get (),
-                          "module/music/panel_lyrics");
-
-    sd->o_text = edje_object_add (evas_object_evas_get (obj));
-    edje_object_file_set (sd->o_text, enna_config_theme_get (),
-                          "module/music/panel_lyrics.textblock");
-
-    sd->o_scroll = elm_scroller_add (sd->o_edje);
-    edje_object_part_swallow(sd->o_edje, "content.swallow", sd->o_scroll);
-    elm_scroller_content_set (sd->o_scroll, sd->o_text);
-
-    evas_object_show (sd->o_edje);
-    evas_object_show (sd->o_scroll);
-    evas_object_show (sd->o_text);
-    evas_object_smart_member_add (sd->o_edje, obj);
-
-    evas_object_smart_data_set (obj, sd);
-}
-
-static void
-_smart_del (Evas_Object *obj)
-{
-    INTERNAL_ENTRY;
-    evas_object_del (sd->o_edje);
-    evas_object_del (sd->o_scroll);
-    evas_object_del (sd->o_text);
-    free (sd);
-}
-
-static void
-_smart_move (Evas_Object *obj, Evas_Coord x, Evas_Coord y)
-{
-    INTERNAL_ENTRY;
-
-    if ((sd->x == x) && (sd->y == y))
-        return;
-
-    sd->x = x;
-    sd->y = y;
-
-    _smart_reconfigure (sd);
-}
-
-static void
-_smart_resize (Evas_Object *obj, Evas_Coord w, Evas_Coord h)
-{
-    INTERNAL_ENTRY;
-
-    if ((sd->w == w) && (sd->h == h))
-        return;
-
-    sd->w = w;
-    sd->h = h;
-
-    _smart_reconfigure (sd);
-}
-
-static void
-_smart_show (Evas_Object *obj)
-{
-    INTERNAL_ENTRY;
-    evas_object_show (sd->o_edje);
-}
-
-static void
-_smart_hide (Evas_Object *obj)
-{
-    INTERNAL_ENTRY;
-    evas_object_hide (sd->o_edje);
-}
-
-static void
-_smart_color_set (Evas_Object *obj, int r, int g, int b, int a)
-{
-    INTERNAL_ENTRY;
-    evas_object_color_set (sd->o_edje, r, g, b, a);
-}
-
-static void
-_smart_clip_set (Evas_Object *obj, Evas_Object *clip)
-{
-    INTERNAL_ENTRY;
-    evas_object_clip_set (sd->o_edje, clip);
-}
-
-static void
-_smart_clip_unset (Evas_Object *obj)
-{
-    INTERNAL_ENTRY;
-    evas_object_clip_unset (sd->o_edje);
-}
-
-static void
-_smart_init (void)
-{
-    static const Evas_Smart_Class sc = {
-        SMART_NAME,
-        EVAS_SMART_CLASS_VERSION,
-        _smart_add,
-        _smart_del,
-        _smart_move,
-        _smart_resize,
-        _smart_show,
-        _smart_hide,
-        _smart_color_set,
-        _smart_clip_set,
-        _smart_clip_unset,
-        NULL,
-        NULL
-    };
-
-    if (!_smart)
-        _smart = evas_smart_class_new (&sc);
-}
 
 /* externally accessible functions */
 
 Evas_Object *
 enna_panel_lyrics_add (Evas *evas)
 {
-    _smart_init ();
-    return evas_object_smart_add (evas, _smart);
+    Smart_Data *sd;
+    Evas_Object *obj;
+    Evas_Object *sc, *lb;
+
+    sd = calloc(1, sizeof(Smart_Data));
+
+    obj = elm_win_inwin_add(enna->win);
+    sc = elm_scroller_add (enna->layout);
+    lb = elm_label_add(enna->layout);
+
+    elm_scroller_content_set (sc, lb);
+    elm_win_inwin_content_set(obj, sc);
+
+    evas_object_show (lb);
+    evas_object_show (sc);
+
+    sd->scroller = sc;
+    sd->text = lb;
+
+    evas_object_data_set(obj, "sd", sd);
+
+    return obj;
 }
 
 /****************************************************************************/
@@ -208,26 +87,23 @@ enna_panel_lyrics_add (Evas *evas)
 void
 enna_panel_lyrics_set_text (Evas_Object *obj, Enna_Metadata *m)
 {
-    Evas_Coord mw, mh;
     buffer_t *buf;
     char *lyrics, *title;
     char *b;
-
-    API_ENTRY return;
+    Smart_Data *sd = evas_object_data_get(obj, "sd");
 
     if (!m)
       return;
 
-    title = enna_metadata_meta_get (m, "title", 1);
+    title = enna_metadata_meta_get(m, "title", 1);
     if (!title)
         return;
 
-    lyrics = enna_metadata_meta_get (m, "lyrics", 1);
+    lyrics = enna_metadata_meta_get(m, "lyrics", 1);
     if (!lyrics)
     {
         free (title);
-        edje_object_part_text_set (sd->o_text, "lyrics.panel.textblock",
-                                   _("No lyrics found ..."));
+        elm_label_label_set(sd->text, _("No lyrics found ..."));
         return;
     }
 
@@ -252,10 +128,5 @@ enna_panel_lyrics_set_text (Evas_Object *obj, Enna_Metadata *m)
     }
     free (lyrics);
 
-    edje_object_part_text_set (sd->o_text,
-                               "lyrics.panel.textblock", buf->buf);
-    buffer_free (buf);
-    edje_object_calc_force (sd->o_text);
-    edje_object_size_min_calc (sd->o_text, &mw, &mh);
-    evas_object_size_hint_min_set (sd->o_text, mw, mh);
+    elm_label_label_set (sd->text, buf->buf);
 }
