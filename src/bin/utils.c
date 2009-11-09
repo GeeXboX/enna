2d0d3ecb51a500822c9f50402e069f4566eaca09
Benjamin Zores <ben@geexbox.org>
2009-11-09 22:01:01 +0100

    remove no longer used switcher code

--- src/bin/utils.c
+++ src/bin/utils.c
@@ -124,40 +124,6 @@ unsigned int enna_util_calculate_font_size(Evas_Coord w, Evas_Coord h)
 
 }
 
-void enna_util_switch_objects(Evas_Object * container, Evas_Object * obj1, Evas_Object * obj2)
-{
-    Evas_Object *s;
-
-    if (!obj1 && !obj2)
-        return;
-
-    if ((obj1 && obj2))
-    {
-        s = edje_object_part_swallow_get(container, "enna.switcher.swallow2");
-        edje_object_part_swallow(container, "enna.switcher.swallow1", obj2);
-        edje_object_part_swallow(container, "enna.switcher.swallow2", obj1);
-        if (s)
-            evas_object_hide(s);
-
-        edje_object_signal_emit(container, "enna,swallow2,default,now", "enna");
-        edje_object_signal_emit(container, "enna,swallow1,state1,now", "enna");
-        edje_object_signal_emit(container, "enna,swallow1,default", "enna");
-        edje_object_signal_emit(container, "enna,swallow2,state2", "enna");
-    }
-    else if (!obj2)
-    {
-        edje_object_part_swallow(container, "enna.switcher.swallow2", obj1);
-        edje_object_signal_emit(container, "enna,swallow2,default,now", "enna");
-        edje_object_signal_emit(container, "enna,swallow2,state2", "enna");
-    }
-    else if (!obj1)
-    {
-        edje_object_part_swallow(container, "enna.switcher.swallow1", obj2);
-        edje_object_signal_emit(container, "enna,swallow1,state1,now", "enna");
-        edje_object_signal_emit(container, "enna,swallow1,default", "enna");
-    }
-}
-
 #define MD5_SIZE 33
 #define MD5_SUM_SIZE 16
 
