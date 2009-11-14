b93e6c01ee11e432e2ceead171d7ef2100edb253
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-11-14 10:40:37 +0100

    cosmetics: remove space between the function's name and '(' in bin/buffer.[ch]

--- src/bin/buffer.c
+++ src/bin/buffer.c
@@ -42,13 +42,13 @@
 #define BUFFER_DEFAULT_CAPACITY 32768
 
 buffer_t *
-buffer_new (void)
+buffer_new(void)
 {
     return calloc(1, sizeof (buffer_t));
 }
 
 void
-buffer_append (buffer_t *buffer, const char *str)
+buffer_append(buffer_t *buffer, const char *str)
 {
     size_t len;
 
@@ -64,16 +64,16 @@ buffer_append (buffer_t *buffer, const char *str)
     len = buffer->len + strlen (str);
     if (len >= buffer->capacity)
     {
-        buffer->capacity = MAX (len + 1, 2 * buffer->capacity);
-        buffer->buf = realloc (buffer->buf, buffer->capacity);
+        buffer->capacity = MAX(len + 1, 2 * buffer->capacity);
+        buffer->buf = realloc(buffer->buf, buffer->capacity);
     }
 
-    strcat (buffer->buf, str);
-    buffer->len += strlen (str);
+    strcat(buffer->buf, str);
+    buffer->len += strlen(str);
 }
 
 void
-buffer_appendf (buffer_t *buffer, const char *format, ...)
+buffer_appendf(buffer_t *buffer, const char *format, ...)
 {
     char str[BUFFER_DEFAULT_CAPACITY];
     int size;
@@ -82,27 +82,27 @@ buffer_appendf (buffer_t *buffer, const char *format, ...)
     if (!buffer || !format)
         return;
 
-    va_start (va, format);
-    size = vsnprintf (str, BUFFER_DEFAULT_CAPACITY, format, va);
+    va_start(va, format);
+    size = vsnprintf(str, BUFFER_DEFAULT_CAPACITY, format, va);
     if (size >= BUFFER_DEFAULT_CAPACITY)
     {
-        char *dynstr = malloc (size + 1);
-        vsnprintf (dynstr, size + 1, format, va);
-        buffer_append (buffer, dynstr);
-        free (dynstr);
+        char *dynstr = malloc(size + 1);
+        vsnprintf(dynstr, size + 1, format, va);
+        buffer_append(buffer, dynstr);
+        free(dynstr);
     }
     else
-        buffer_append (buffer, str);
-    va_end (va);
+        buffer_append(buffer, str);
+    va_end(va);
 }
 
 void
-buffer_free (buffer_t *buffer)
+buffer_free(buffer_t *buffer)
 {
     if (!buffer)
         return;
 
     if (buffer->buf)
-        free (buffer->buf);
-    free (buffer);
+        free(buffer->buf);
+    free(buffer);
 }
--- src/bin/buffer.h
+++ src/bin/buffer.h
@@ -36,10 +36,10 @@ typedef struct buffer_s {
     size_t capacity;
 } buffer_t;
 
-buffer_t *buffer_new (void);
-void buffer_free (buffer_t *buffer);
+buffer_t *buffer_new(void);
+void buffer_free(buffer_t *buffer);
 
-void buffer_append (buffer_t *buffer, const char *str);
-void buffer_appendf (buffer_t *buffer, const char *format, ...);
+void buffer_append(buffer_t *buffer, const char *str);
+void buffer_appendf(buffer_t *buffer, const char *format, ...);
 
 #endif /* BUFFER_H */
