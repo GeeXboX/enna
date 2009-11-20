519e8a837d97e85a04c2a932d40c24f9ffc6b825
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-11-20 20:25:05 +0100

    disable signals with libcurl

--- src/bin/url_utils.c
+++ src/bin/url_utils.c
@@ -108,6 +108,7 @@ url_get_data(url_t handler, char *url)
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, url_buffer_get);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
     curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
 
     chunk.status = curl_easy_perform(curl);
 
