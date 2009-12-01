1d2fb653e1e178a98fe90493fba80a290735173b
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-12-01 17:55:36 +0100

    add SExEP and sSEeEP keywords for the decrapifier

--- src/bin/enna_config.c
+++ src/bin/enna_config.c
@@ -150,7 +150,7 @@
     "scan_priority=19\n" \
     "\n" \
     "# blacklisted keywords\n" \
-    "blacklist_keywords=0tv,1080p,2hd,720p,ac3,booya,caph,crimson,ctu,dimension,divx,dot,dsr,dvdrip,dvdscr,e7,etach,fov,fqm,hdq,hdtv,lol,mainevent,notv,pdtv,proper,pushercrew,repack,reseed,screencam,screener,sys,vtv,x264,xor,xvid\n" \
+    "blacklist_keywords=0tv,1080p,2hd,720p,ac3,booya,caph,crimson,ctu,dimension,divx,dot,dsr,dvdrip,dvdscr,e7,etach,fov,fqm,hdq,hdtv,lol,mainevent,notv,pdtv,proper,pushercrew,repack,reseed,screencam,screener,sys,vtv,x264,xor,xvid,SExEP,sSEeEP\n" \
     "\n" \
 
 
