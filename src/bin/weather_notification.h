31c993da3fe62df92951570b5d4686bc64bd2260
Benjamin Zores <ben@geexbox.org>
2009-12-29 14:29:51 +0100

    declare missing prototype

--- src/bin/weather_notification.h
+++ src/bin/weather_notification.h
@@ -23,5 +23,6 @@
 #define WEATHER_NOTIFICATION_H
 
 Evas_Object *enna_weather_notification_smart_add (Evas *evas);
+void enna_weather_notification_update (Evas_Object *obj);
 
 #endif /* WEATHER_NOTIFICATION_H */
