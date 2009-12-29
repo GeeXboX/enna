6c4ba929240c6fc01de217aefb9b2880519e0315
Benjamin Zores <ben@geexbox.org>
2009-12-29 12:42:14 +0100

    avoid libplayer warnings if trying to set volume to incoherent values

--- src/bin/mediaplayer.c
+++ src/bin/mediaplayer.c
@@ -1280,6 +1280,8 @@ void
 enna_mediaplayer_default_increase_volume(void)
 {
     int vol = enna_mediaplayer_volume_get();
+    if ((vol + VOLUME_STEP_DEFAULT) >= 100)
+        return;
     player_audio_volume_set(mp->player, vol + VOLUME_STEP_DEFAULT);
 }
 
@@ -1287,6 +1289,8 @@ void
 enna_mediaplayer_default_decrease_volume(void)
 {
     int vol = enna_mediaplayer_volume_get();
+    if ((vol - VOLUME_STEP_DEFAULT) <= 0)
+        return;
     player_audio_volume_set(mp->player, vol - VOLUME_STEP_DEFAULT);
 }
 
