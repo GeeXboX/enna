f7f96c81242f7518aed24721d47fdfac13c652ce
Davide Cavalca <davide@geexbox.org>
2010-02-08 12:52:30 +0100

    clarify copyright of gettext.h and switch its license as a whole to LGPL 2.1 like the rest of Enna

--- src/bin/gettext.h
+++ src/bin/gettext.h
@@ -1,19 +1,20 @@
 /* Convenience header for conditional use of GNU <libintl.h>.
+   Adapted for The Enna Project from gnulib-local/lib/gettext.h
+   in gettext-0.17 sources.
    Copyright (C) 1995-1998, 2000-2002, 2004-2006 Free Software Foundation, Inc.
-
-   Adapted for The Enna Project by Davide Cavalca <davide@geexbox.org>.
-
+   Copyright (C) 2005-2010 The Enna Project
+   
    This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Library General Public License as published
-   by the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation; either version 2.1 of the License, or 
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Library General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
+   You should have received a copy of the GNU Lesser General Public
    License along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
    USA.  */
