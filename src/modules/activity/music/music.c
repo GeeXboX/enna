de88899df46d8a90a802177f19061c781503750b
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2011-01-19 18:23:31 +0100

    Show mediaplayer after populate the list

--- src/modules/activity/music/music.c
+++ src/modules/activity/music/music.c
@@ -227,11 +227,13 @@ _browser_selected_cb(void *data, Evas_Object *obj, void *event_info)
                      enna_mediaplayer_select_nth(mod->enna_playlist,i);
                      enna_mediaplayer_obj_event_catch(mod->o_mediaplayer);
                      enna_mediaplayer_play(mod->enna_playlist);
-                     _panel_infos_display(0);
+
                  }
                  i++;
              }
          }
+         
+         _panel_infos_display(0);
     }
 }
 
